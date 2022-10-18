1037a07f409563e7a951420db53ff6d84841ff8a
Ignacio Valenzuela M <46540216+IgnacioValenzuelaM@users.noreply.github.com>
2022-10-18 15:28:11 -0300

    Update smina_redocking.py

--- smina_redocking.py
+++ smina_redocking.py
@@ -13,3 +13,12 @@ import streamlit as st
 """
 
 with st.echo(code_location='below'):
+    from stmol import showmol
+    import py3Dmol
+    # 1A2C
+    # Structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA
+    xyzview = py3Dmol.view(query='pdb:1A2C') 
+    xyzview.setStyle({'cartoon':{'color':'spectrum'}})
+    showmol(xyzview, height = 500,width=800)
+    from stmol import *
+    showmol(render_pdb(id = '1A2C'))
